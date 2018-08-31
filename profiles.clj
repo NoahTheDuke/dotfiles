da08e8a77be129c20e1d15963e91de62da733ea9
Noah <nbtheduke@gmail.com>
2018-08-31 00:16:42 -0400

    Final claritas setting

--- profiles.clj
+++ profiles.clj
@@ -1,8 +1,12 @@
-{:user {
-        ; :dependencies [[pjstadig/humane-test-output "0.8.3"]]
-        ; :injections [(require 'pjstadig.humane-test-output)
-        ;              (pjstadig.humane-test-output/activate!)]
-        :plugins [[com.jakemccrary/lein-test-refresh "0.22.0"]
-                  [lein-kibit "0.1.5"]]
+{:user {:plugins [[cider/cider-nrepl "0.17.0"]
+                  [com.jakemccrary/lein-test-refresh "0.22.0"]
+                  [jonase/eastwood "0.2.5"]
+                  [lein-cljfmt "0.5.7"]
+                  [lein-topology "0.2.0"]
+                  [lein-gossip "0.1.0-SNAPSHOT"]
+                  [lein-kibit "0.1.5"]
+                  [venantius/ultra "0.5.2"]]
+        :eastwood {:exclude-linters [:no-ns-form-found]}
         :test-refresh {:changes-only true
-                       :quiet true}}}
+                       :quiet true
+                       :watch-dirs ["test"]}}}
