020def803ba5b576e70c3a8face3ae34b561bcc1
Noah Bogart <noah.bogart@ebth.com>
2018-09-13 14:50:28 -0400

    Updated with ruby/ssh/ebth settings

--- git/hooks/pre-commit.bash
+++ git/hooks/pre-commit.bash
@@ -1,13 +1,13 @@
-STATUS = 0
+STATUS=0
 
 for focus in fdescribe fcontext fit fspecify fexample; do
-	if [ $(git diff --staged | rg "^\+\s*$focus\(") ]
-	then
-		echo "You forgot to remove a $focus in the following files:"
-		git diff --staged --name-only -G"^\s*$focus\("
-		echo ""
-		STATUS=1
-	fi
+    if [ $(git diff --staged | rg "^\+\s*$focus\(") ]
+    then
+        echo "You forgot to remove a $focus in the following files:"
+        git diff --staged --name-only -G"^\s*$focus\("
+        echo ""
+        STATUS=1
+    fi
 done
 
 exit $STATUS
