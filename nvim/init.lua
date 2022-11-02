8660b443d462926132fbf0f6a72418c5e1802c53
Noah Bogart <nbogart@crossbeam.com>
2022-11-01 22:34:22 -0400

    Small fixes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 -- vim.cmd [[
 --   set runtimepath^=$HOME/personal/coc-clojure
 --   set runtimepath^=$HOME/personal/coc.nvim
---   source $HOME/dotfiles/nvim/coc-nvim.vim
+--   lua require "plugins.coc-nvim"
 --   let $NVIM_COC_LOG_LEVEL='debug'
 --   let $NVIM_COC_LOG_FILE = '/tmp/coc-nvim.log'
 --   let g:coc_node_args = ['-r', expand('~/.config/yarn/global/node_modules/source-map-support/register')]
--- nvim/lua/plugins/conjure.lua
+++ nvim/lua/plugins/conjure.lua
@@ -1,8 +1,9 @@
-vim.g["conjure#mapping#def_word"] = false
-vim.g["conjure#mapping#doc_word"] = false
 vim.g["conjure#client#clojure#nrepl#eval#raw_out"] = true
-vim.g["conjure#client#clojure#nrepl#test#runner"] = "kaocha"
 vim.g["conjure#client#clojure#nrepl#test#call_suffix"] = "{:kaocha/color? false :kaocha/reporter [kaocha.report/result] :capture-output? false}"
 vim.g["conjure#client#clojure#nrepl#test#current_form_names"] = {"deftest", "defexpect"}
-vim.g["conjure#log#wrap"] = true
+vim.g["conjure#client#clojure#nrepl#test#runner"] = "kaocha"
 vim.g["conjure#extract#tree_sitter#enabled"] = true
+vim.g["conjure#log#hud#ignore_low_priority"] = true
+vim.g["conjure#log#wrap"] = true
+vim.g["conjure#mapping#def_word"] = false
+vim.g["conjure#mapping#doc_word"] = false
