f2317ac33cc93c394c5e767e070a1cf273ee086a
Noah Bogart <noah.bogart@hey.com>
2024-02-14 10:31:02 -0500

    Update lazy-lock

--- nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/lua/plugins/nvim-autopairs.lua
@@ -2,7 +2,7 @@
 local utils = require("utils")
 local function _1_()
   local autopairs = require("nvim-autopairs")
-  autopairs.setup({check_ts = true, ts_config = {lua = {"string"}, javascript = {"string", "template_string"}}, map_cr = false, break_undo = false, enable_check_bracket_line = false})
+  autopairs.setup({check_ts = true, ts_config = {lua = {"string"}, javascript = {"string", "template_string"}}, break_undo = false, enable_check_bracket_line = false, map_cr = false})
   do end ((autopairs.get_rules("'"))[1])["not_filetypes"] = {"scheme", "lisp", "clojure"}
   (autopairs.get_rules("`"))[1]["not_filetypes"] = {"scheme", "lisp", "clojure"}
   return nil
