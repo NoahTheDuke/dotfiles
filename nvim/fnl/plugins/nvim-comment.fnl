772cd3c561946905ea9b891fe71f67d99d84c511
Noah Bogart <noah.bogart@hey.com>
2024-05-17 16:44:18 -0400

    Remove nvim-comment

--- nvim/fnl/plugins/nvim-comment.fnl
+++
@@ -1,9 +0,0 @@
-(local utils (require "utils"))
-
-;; gcc
-(utils.dep
-  "https://github.com/terrortylor/nvim-comment"
-  {:config
-   (fn []
-     (let [nvim-comment (require "nvim_comment")]
-       (nvim-comment.setup {:comment_empty false})))})
--- nvim/lua/plugins/nvim-comment.lua
+++
@@ -1,7 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/nvim-comment.fnl by https://github.com/Olical/nfnl, do not edit.
-local utils = require("utils")
-local function _1_()
-  local nvim_comment = require("nvim_comment")
-  return nvim_comment.setup({comment_empty = false})
-end
-return utils.dep("https://github.com/terrortylor/nvim-comment", {config = _1_})
