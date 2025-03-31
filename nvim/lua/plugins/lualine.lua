976cc133904f893fba3ff59f2f1f0382a40e3d0e
Noah Bogart <nbogart@relaynetwork.com>
2025-03-31 11:46:24 -0400

    Move from Lualine to built-in statusline

--- nvim/lua/plugins/lualine.lua
+++
@@ -1,8 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/lualine.fnl by https://github.com/Olical/nfnl, do not edit.
-local utils = require("utils")
-local function config()
-  local lualine = require("lualine")
-  return lualine.setup({options = {component_separators = {left = "\238\130\176", right = "\238\130\178"}, section_separators = {left = "\238\130\176", right = "\238\130\178"}, theme = "auto"}, sections = {lualine_a = {"mode"}, lualine_b = {{"filename", file_status = true, path = 1, symbols = {modified = " \239\129\177 ", readonly = "[-]", unnamed = "[No Name]"}}, {"diagnostics", sources = {"coc"}}, {"g:coc_status", "bo:filetype"}}, lualine_c = {}, lualine_x = {"encoding"}, lualine_y = {{"filetype", colored = true}}, lualine_z = {{"%3p%%/%4L %4l:%3v"}}}, extensions = {"quickfix", "nvim-tree"}})
-end
---[[ (config) ]]
-return {utils.dep("https://github.com/nvim-lualine/lualine.nvim", {config = config})}
---
+++ nvim/lua/statusline.lua
@@ -0,0 +1,18 @@
+-- [nfnl] Compiled from fnl/statusline.fnl by https://github.com/Olical/nfnl, do not edit.
+_G.Statusline = {}
+vim.cmd("\nset laststatus=2\nset statusline=\nset statusline+=%1*\nset statusline+=\\ \nset statusline+=%{v:lua._G.Statusline.Mode()}\nset statusline+=\\ \nset statusline+=%2*\nset statusline+=\\ \nset statusline+=%F\nset statusline+=\\ \nset statusline+=%m\nset statusline+=%r\nset statusline+=%3*\nset statusline+=\\ \nset statusline+=%{coc#status()}\nset statusline+=%=\nset statusline+=\\ \nset statusline+=%2*\nset statusline+=\\ \nset statusline+=%{&fenc==\\\"\\\"?&enc:&fenc}\nset statusline+=\\ \nset statusline+=%y\nset statusline+=\\ \nset statusline+=%1*\nset statusline+=\\ \nset statusline+=l%l:c%2v,\\ %2p%%\\ of\\ %2L\\ Lines\nset statusline+=\\ \n")
+_G.currentmode = {n = "NORMAL", no = "NORMALOP", v = "VISUAL", V = "VISUAL-LINE", ["^V"] = "VISUAL-BLOCK", s = "SELECT", S = "SELECT-LINE", ["^S"] = "SELECT-BLOCK", i = "INSERT", R = "REPLACE", Rv = "VISUAL-REPLACE", c = "COMMAND", cv = "VIM EX", ce = "EX", r = "PROMPT", rm = "MORE", ["r?"] = "CONFIRM", ["!"] = "SHELL", t = "TERMINAL"}
+_G.Statusline.Mode = function()
+  local mode = vim.api.nvim_call_function("mode", {})
+  local _2_
+  do
+    local t_1_ = _G.currentmode
+    if (nil ~= t_1_) then
+      t_1_ = t_1_[mode]
+    else
+    end
+    _2_ = t_1_
+  end
+  return (_2_ or "")
+end
+return nil
