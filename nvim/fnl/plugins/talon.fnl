5773c24f4e6b90a119464d13cda625453cf2da14
Noah Bogart <noah.bogart@hey.com>
2025-06-11 12:19:31 -0400

    csvview and talon

---
+++ nvim/fnl/plugins/talon.fnl
@@ -0,0 +1,5 @@
+(local utils (require :utils))
+
+(utils.dep
+  "https://github.com/seanyeh/talon.vim"
+  {})
---
+++ nvim/lua/plugins/talon.lua
@@ -0,0 +1,3 @@
+-- [nfnl] nvim/fnl/plugins/talon.fnl
+local utils = require("utils")
+return utils.dep("https://github.com/seanyeh/talon.vim", {})
