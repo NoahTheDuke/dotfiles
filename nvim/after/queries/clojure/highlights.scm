e6bcb566b572370caf1a8f2adf967f9454adb30c
Noah Bogart <noah.bogart@hey.com>
2025-12-09 10:29:49 -0500

    remove clojure highlights

--- nvim/after/queries/clojure/highlights.scm
+++ nvim/after/queries/clojure/highlights.scm
@@ -1,18 +1,5 @@
 ; extends
 
-; General function calls
-(list_lit
-  meta: (_)?
-  .
-  value:
-  (sym_lit) @function.call)
-
-(anon_fn_lit
-  meta: (_)?
-  .
-  value:
-  (sym_lit) @function.call)
-
 (var_quoting_lit) @punctuation.special
 (derefing_lit) @punctuation.special
 
