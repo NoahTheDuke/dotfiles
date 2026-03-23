32320b73757b028f2236de52ca832c65cbda77c9
Noah Bogart <noah.bogart@hey.com>
2026-03-23 16:17:56 -0400

    clean up alpha, bookmark-advance-to in jj

--- nvim/fnl/noahtheduke/plugins/alpha.fnl
+++ nvim/fnl/noahtheduke/plugins/alpha.fnl
@@ -1,19 +1,29 @@
 (local utils (require "noahtheduke.utils"))
 (import-macros {: when-require} "noahtheduke.util-macros")
 
+(λ ->footer [?lazy-plugins]
+  (let [vim-version (vim.version)
+        version (.. " " vim-version.major "." vim-version.minor "." vim-version.patch)]
+    (if (= nil ?lazy-plugins)
+      version
+      (let [cnt (length ?lazy-plugins)]
+        (.. version "   " cnt " Plugins")))))
+
+	
+
 (λ setup []
   (when-require [alpha "alpha"
                  dashboard "alpha.themes.dashboard"
                  headers "utils.headers"]
-    (let [lazy_plugins
-          (vim.tbl_map (λ [plugin] plugin.name)
-                       (let [lazy (require "lazy")]
-                         (lazy.plugins)))]
+    (math.randomseed (os.time))
+    (let [{: header
+           : buttons
+           : footer} (vim.deepcopy dashboard.section)]
 
-      (math.randomseed (os.time))
-      (set dashboard.section.header.val (headers.random))
+      (set header.val (headers.random))
+      (set header.opts.hl "AlphaHeader")
 
-      (set dashboard.section.buttons.val
+      (set buttons.val
            [(dashboard.button "s" "  Load Session" ":SessionManager load_current_dir_session<CR>")
             (dashboard.button "r" "  Recent Files" ":Telescope oldfiles<CR>")
             (dashboard.button "n" "  New file" ":ene <BAR> startinsert <CR>")
@@ -21,24 +31,28 @@
             (dashboard.button "t" "  Find Text" ":Telescope live_grep<CR>")
             (dashboard.button "c" "  Configuration" ":e $MYVIMRC<CR>:cd %:h/../<CR>")
             (dashboard.button "q" "  Quit Neovim" ":qa!<CR>")])
+      (set buttons.opts.hl "AlphaButton")
 
-      (λ footer []
-        (let [vim-version (vim.version)
-              version (.. " " vim-version.major "." vim-version.minor "." vim-version.patch)]
-          (if (= lazy_plugins nil)
-            version
-            (let [cnt (length lazy_plugins)
-                  total_plugins (.. "   " cnt " Plugins")]
-              (.. version total_plugins)))))
+      (let [lazy-plugins (vim.tbl_map (λ [plugin] plugin.name)
+                                      (let [lazy (require "lazy")]
+                                        (lazy.plugins)))]
+        (set footer.val (->footer lazy-plugins)))
+      (set footer.opts.hl "AlphaFooter")
 
-      (set dashboard.section.footer.val (footer))
+      (let [config
+            {:layout [{:type :padding :val 2}
+                      header
+                      {:type :padding :val 2}
+                      {:type :text
+                       :val "poop"
+                       :opts {:position "center"}}
+                      {:type :padding :val 2}
+                      buttons
+                      footer]
+             :opts {:margin 5
+                    :noautocmd true}}]
 
-      (set dashboard.section.footer.opts.hl "AlphaFooter")
-      (set dashboard.section.header.opts.hl "AlphaHeader")
-      (set dashboard.section.buttons.opts.hl "AlphaButton")
-
-      (set dashboard.opts.opts.noautocmd true)
-      (alpha.setup dashboard.opts))))
+        (alpha.setup config)))))
 
 ;; Splash screen
 (utils.dep
--- nvim/lua/noahtheduke/plugins/alpha.lua
+++ nvim/lua/noahtheduke/plugins/alpha.lua
@@ -1,86 +1,91 @@
 -- [nfnl] fnl/noahtheduke/plugins/alpha.fnl
 local utils = require("noahtheduke.utils")
-local function setup()
-  local errors_1_ = {}
-  local _2_, alpha
-  local function _5_()
-    return require("alpha")
-  end
-  local function _6_(err_2_auto)
-    if (nil == err_2_auto) then
-      _G.error("Missing argument err_2_auto on fnl/noahtheduke/plugins/alpha.fnl:5", 2)
-    else
-    end
-    return table.insert(errors_1_, debug.traceback(err_2_auto))
-  end
-  _2_, alpha = xpcall(_5_, _6_)
-  local _3_, dashboard
-  local function _8_()
-    return require("alpha.themes.dashboard")
-  end
-  local function _9_(err_2_auto)
-    if (nil == err_2_auto) then
-      _G.error("Missing argument err_2_auto on fnl/noahtheduke/plugins/alpha.fnl:5", 2)
-    else
-    end
-    return table.insert(errors_1_, debug.traceback(err_2_auto))
-  end
-  _3_, dashboard = xpcall(_8_, _9_)
-  local _4_, headers
-  local function _11_()
-    return require("utils.headers")
-  end
-  local function _12_(err_2_auto)
-    if (nil == err_2_auto) then
-      _G.error("Missing argument err_2_auto on fnl/noahtheduke/plugins/alpha.fnl:5", 2)
-    else
-    end
-    return table.insert(errors_1_, debug.traceback(err_2_auto))
-  end
-  _4_, headers = xpcall(_11_, _12_)
-  if (_2_ and _3_ and _4_) then
-    local lazy_plugins
-    local function _14_(plugin)
-      if (nil == plugin) then
-        _G.error("Missing argument plugin on fnl/noahtheduke/plugins/alpha.fnl:9", 2)
-      else
-      end
-      return plugin.name
-    end
-    local function _16_()
-      local lazy = require("lazy")
-      return lazy.plugins()
-    end
-    lazy_plugins = vim.tbl_map(_14_, _16_())
-    math.randomseed(os.time())
-    dashboard.section.header.val = headers.random()
-    dashboard.section.buttons.val = {dashboard.button("s", "\239\157\138  Load Session", ":SessionManager load_current_dir_session<CR>"), dashboard.button("r", "\239\152\136  Recent Files", ":Telescope oldfiles<CR>"), dashboard.button("n", "\239\133\155  New file", ":ene <BAR> startinsert <CR>"), dashboard.button("f", "\239\144\162  Find File", ":Telescope find_files<CR>"), dashboard.button("t", "\239\158\131  Find Text", ":Telescope live_grep<CR>"), dashboard.button("c", "\238\152\149  Configuration", ":e $MYVIMRC<CR>:cd %:h/../<CR>"), dashboard.button("q", "\239\153\153  Quit Neovim", ":qa!<CR>")}
-    local function footer()
-      local vim_version = vim.version()
-      local version = ("\239\148\167 " .. vim_version.major .. "." .. vim_version.minor .. "." .. vim_version.patch)
-      if (lazy_plugins == nil) then
-        return version
-      else
-        local cnt = #lazy_plugins
-        local total_plugins = ("  \239\150\149 " .. cnt .. " Plugins")
-        return (version .. total_plugins)
-      end
-    end
-    dashboard.section.footer.val = footer()
-    dashboard.section.footer.opts.hl = "AlphaFooter"
-    dashboard.section.header.opts.hl = "AlphaHeader"
-    dashboard.section.buttons.opts.hl = "AlphaButton"
-    dashboard.opts.opts.noautocmd = true
-    return alpha.setup(dashboard.opts)
+local function __3efooter(_3flazy_plugins)
+  local vim_version = vim.version()
+  local version = ("\239\148\167 " .. vim_version.major .. "." .. vim_version.minor .. "." .. vim_version.patch)
+  if (nil == _3flazy_plugins) then
+    return version
   else
-    return vim.notify(errors_1_[1], vim.log.levels.ERROR)
+    local cnt = #_3flazy_plugins
+    return (version .. "  \239\150\149 " .. cnt .. " Plugins")
   end
 end
-local function _19_(_plugin, _opts, run)
+local function setup()
+  local errors_2_ = {}
+  local _3_, alpha
+  local function _6_()
+    return require("alpha")
+  end
+  local function _7_(err_2_auto)
+    if (nil == err_2_auto) then
+      _G.error("Missing argument err_2_auto on fnl/noahtheduke/plugins/alpha.fnl:15", 2)
+    else
+    end
+    return table.insert(errors_2_, debug.traceback(err_2_auto))
+  end
+  _3_, alpha = xpcall(_6_, _7_)
+  local _4_, dashboard
+  local function _9_()
+    return require("alpha.themes.dashboard")
+  end
+  local function _10_(err_2_auto)
+    if (nil == err_2_auto) then
+      _G.error("Missing argument err_2_auto on fnl/noahtheduke/plugins/alpha.fnl:15", 2)
+    else
+    end
+    return table.insert(errors_2_, debug.traceback(err_2_auto))
+  end
+  _4_, dashboard = xpcall(_9_, _10_)
+  local _5_, headers
+  local function _12_()
+    return require("utils.headers")
+  end
+  local function _13_(err_2_auto)
+    if (nil == err_2_auto) then
+      _G.error("Missing argument err_2_auto on fnl/noahtheduke/plugins/alpha.fnl:15", 2)
+    else
+    end
+    return table.insert(errors_2_, debug.traceback(err_2_auto))
+  end
+  _5_, headers = xpcall(_12_, _13_)
+  if (_3_ and _4_ and _5_) then
+    math.randomseed(os.time())
+    local _let_15_ = vim.deepcopy(dashboard.section)
+    local header = _let_15_.header
+    local buttons = _let_15_.buttons
+    local footer = _let_15_.footer
+    header.val = headers.random()
+    header.opts.hl = "AlphaHeader"
+    buttons.val = {dashboard.button("s", "\239\157\138  Load Session", ":SessionManager load_current_dir_session<CR>"), dashboard.button("r", "\239\152\136  Recent Files", ":Telescope oldfiles<CR>"), dashboard.button("n", "\239\133\155  New file", ":ene <BAR> startinsert <CR>"), dashboard.button("f", "\239\144\162  Find File", ":Telescope find_files<CR>"), dashboard.button("t", "\239\158\131  Find Text", ":Telescope live_grep<CR>"), dashboard.button("c", "\238\152\149  Configuration", ":e $MYVIMRC<CR>:cd %:h/../<CR>"), dashboard.button("q", "\239\153\153  Quit Neovim", ":qa!<CR>")}
+    buttons.opts.hl = "AlphaButton"
+    do
+      local lazy_plugins
+      local function _16_(plugin)
+        if (nil == plugin) then
+          _G.error("Missing argument plugin on fnl/noahtheduke/plugins/alpha.fnl:36", 2)
+        else
+        end
+        return plugin.name
+      end
+      local function _18_()
+        local lazy = require("lazy")
+        return lazy.plugins()
+      end
+      lazy_plugins = vim.tbl_map(_16_, _18_())
+      footer.val = __3efooter(lazy_plugins)
+    end
+    footer.opts.hl = "AlphaFooter"
+    local config = {layout = {{type = "padding", val = 2}, header, {type = "padding", val = 2}, {type = "text", val = "poop", opts = {position = "center"}}, {type = "padding", val = 2}, buttons, footer}, opts = {margin = 5, noautocmd = true}}
+    return alpha.setup(config)
+  else
+    return vim.notify(errors_2_[1], vim.log.levels.ERROR)
+  end
+end
+local function _20_(_plugin, _opts, run)
   if run then
     return setup()
   else
     return nil
   end
 end
-return utils.dep("https://github.com/goolord/alpha-nvim", {config = _19_})
+return utils.dep("https://github.com/goolord/alpha-nvim", {config = _20_})
